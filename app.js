d321eb2d7ac30ef38609b9b69ddfaf6345023834
nola360 <adaramolajr@gmail.com>
2020-02-29 03:01:06 -0500

    recreated project with different color, still tweaking

---
+++ app.js
@@ -0,0 +1,9 @@
+function openSM() {
+  document.getElementById('sideMenu-id').style.width = '450px';
+  document.getElementById('page-content').style.marginLeft = '450px';
+}
+
+function closeSM() {
+  document.getElementById('sideMenu-id').style.width = '0';
+  document.getElementById('page-content').style.marginLeft = '0';
+}
